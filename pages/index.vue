7616defbd425c9292f00976fbd541b4104a23624
Jesutooni <edeworuyoyou@gmail.com>
2022-07-23 12:30:30 +0100

    project setup

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-primary text-4xl text-center py-12 font-bold">Brimble Landing Page is Live</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
