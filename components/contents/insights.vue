0e61534ae9944bb91e7ca7ca593a85bf2fee5b29
Jesutooni <edeworuyoyou@gmail.com>
2022-07-24 12:27:11 +0100

    screen conversion

--- components/contents/insights.vue
+++ components/contents/insights.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="max-w-6xl mx-auto pt-24 px-4 flex justify-between">
-    <div class="space-y-8 max-w-[375px]">
+  <section class="max-w-6xl mx-auto pt-24 px-4 flex flex-col md:flex-row justify-between gap-20">
+    <div class="space-y-8 max-w-[375px] lg:py-24 relative z-10">
       <h1 class="text-[32px] md:text-4xl text-secondary">
         Get important <span class="text-primary">Usage Insights</span>
       </h1>
@@ -17,12 +17,15 @@
       </button>
     </div>
 
-    <div class="relative">
-      <img src="/img/insights-bg.svg" />
+    <div class="relative hidden sm:block">
+      <img class="h-auto lg:h-[90vh]" src="/img/analytics.png" />
 
-      <div class="absolute bottom-0">
-        <img src="/img/analytics.svg" />
+      <div class="absolute bottom-20 -left-40">
+        <img src="/img/stats.svg">
       </div>
+    </div>
+    <div class="sm:hidden">
+      <img class="w-full" src="/img/analytics-mobile.svg">
     </div>
   </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <main>
       <Hero />
       <ContentsAbout />
-      <!-- <ContentsInsights /> -->
+      <ContentsInsights />
       <ContentsStructure />
       <ContentsCollaboration />
       <ContentsCli />
